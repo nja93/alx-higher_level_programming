bec9f0c4b136724df406ae9411a827ac8ac5c714
nja93 <njanjalchege@gmail.com>
2023-05-08 14:27:23 -0700

    function in C that checks if a singly linked list has a cycle in i

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,7 +5,7 @@
  *check_cycle -checks if a signly linked list  has a cycle in it
  * by chege
  * @list: pointer to head of the linked list
- * Return: if 1 has a cycle. 0 is there is nocycle
+ * Return: 1 has a cycle. 0 is there is nocycle
  */
 
 int check_cycle(listint_t *list)
@@ -16,7 +16,7 @@ int check_cycle(listint_t *list)
 		return (0);
 
 	slow = list;
-	fast - list->next;
+	fast = list->next;
 
 		while (fast != NULL && fast->next != NULL)
 		{
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,16 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
+#include <stdlib.h> 
 /**
- * struct listint_s - this is a singlt linked list
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
- * @n: int
-*/
+ *
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 int check_cycle(listint_t *list);
