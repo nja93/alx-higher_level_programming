1efc42e5234c86dc50372c46c4f3d1e2f7793772
nja93 <njanjalchege@gmail.com>
2023-05-11 14:39:01 -0700

    FIXED program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,20 +7,14 @@ if __name__ == "__main__":
     if argc != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    ops = {
-        '+': add,
-        '-': sub,
-        '*': mul,
-        '/': div
-    }
-    if argv[2] in ops:
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = ops[argv[2]]
-        result = op(num1, num2)
-        print("{:d} {:s} {:d} = {:d}".format(num1, argv[2], num2, result))
+    operators = {'+': add, "-": sub, '*': mul, '/': div}
+if argv[2] in ops:
+        number1 = int(argv[1])
+        number2 = int(argv[3])
+        ops = operators[argv[2]]
+        result = ops(number1, number2)
+        print("{:d} {:s} {:d} = {:d}".format(number1, argv[2], number2, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     exit(0)
-
