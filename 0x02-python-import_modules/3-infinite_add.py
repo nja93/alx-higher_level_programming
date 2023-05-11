abb92cf8f5ce4413d1b4abd72333fe67af80955f
nja93 <njanjalchege@gmail.com>
2023-05-11 19:20:34 +0000

    fixed program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    c = len(argv) - 1
-    if c == 0:
-        print("{:d}". format(c))
-        return
-    else:
-        x = 1
-        add = 0
-        while x <= c:
-            add = add + int(argv[x])
-            x += 1
-            print("{:d}".format(add))
-    
-    if __name__ == "__main__":
-        import sys
-        add_arg(sys.argv)
+
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    for r in range (len(sys.argv) - 1):
+        total += int(sys.argv[r + 1])
+    print("{}".format(total))
