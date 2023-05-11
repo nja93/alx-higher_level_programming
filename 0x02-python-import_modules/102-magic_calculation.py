6bfc99554e272a9f0af2a8ebe442f0f268a07b64
nja93 <njanjalchege@gmail.com>
2023-05-11 14:16:39 -0700

    program that imports all functions from the file calculator_1.py and handles basic operations.

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from magic_calculation_102 import sub, add
+
+
+def maginc_calculation(a, b):
+    if a < b:
+         c = add(a, b)
+         for r in range(4,6):
+             z = add(z, r)
+            return(z)
+ else:
+     return sub(a, b)
