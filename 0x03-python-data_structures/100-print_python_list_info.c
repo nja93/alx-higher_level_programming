02a161c27bbd61e1eac512c3194f5a70798013d1
nja93 <njanjalchege.com>
2023-05-15 22:32:40 +0000

    C function that prints some basic info about Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,38 @@
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
 /**
- *  * print_python_list_info - Prints information about python objects
- * @p: PyObject pointer 
- *by Lorna
+ * print_python_list_info - funtion that prints basic info about python list
+* Authored by Lorna Chege 
+* @p: the python object
  */
+
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, x;
+	Py_ssize_t size;
+	Py_ssize_t x;
 	PyObject *item;
+	PyListObject *list;
+
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "Error:Object is not a list.\n");
+		return;
+	}
 
 	size = PyList_Size(p);
-	printf("[*] Size of Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+	list = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", list->allocated);
+
 
 	for (x = 0; x < size; x++)
 	{
-	item = PyList_GetItem(p, x);
-	printf("Element %zd: %s\n", x, Py_TYPE(item)->tp_name);
+		item = list->ob_item[x];
+		printf("Element %zd: %s\n", x, Py_TYPE(item)->tp_name);
 	}
+	if (PyErr_Occurred())
+		PyErr_Print();
 }
