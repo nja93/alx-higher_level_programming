f33aa8e0aca59208f087940a90559b12a4a49fa3
nja93 <njanjalchege@gmail.com>
2023-05-15 11:28:17 +0000

    function that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx <= len(my_list):
         return my_list
     else:
-    new_list = mylist.copy()
-    new_list[idx] = element
-    return new_list
+        new_list = mylist.copy()
+        new_list[idx] = element
+            return new_list
