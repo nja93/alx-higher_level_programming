be9de4370ee7a100c47c798cca53cd7226092d13
nja93 <njanjalchege@gmail.com>
2023-05-18 11:48:13 +0000

    function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    multiplied_dict_key = {}
-    for key, value in a_dictionary.items():
-        multiplied_dict_key[key] = value * 2
-           return multiplied_dict_key
+    new_dictionary = a_dictionary.copy()
+    for value in new_dictionary:
+        new_dictionary[value] *= 2
+    return new_dictionary
