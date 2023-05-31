3a2b0fdb14fb70dde9331a16cb98dda55c398a2b
nja93 <njanjalchege@gmail.com>
2023-05-31 06:17:05 -0700

    FIXED function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except(ValueError, TypeError)as errors:
+    except(TypeError, ValueError)as errors:
         sys.stderr.write("Exception: {}\n".format(errors))
         return False
