3c9617483d54e4126023d39d3670ff673b6170b4
nja93 <njanjalchege@gmail.com>
2023-05-31 05:07:13 -0700

    FIXED  function that prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
-        for item in range(x):
-            print(my_list[item],end="")
+        count = 0
+        for item in range(0, x):
+            print("{}".format(my_list[item]), end="")
             count += 1
     except IndexError:
         pass
-print()
-return count
+    finally:
+        print()
+        return count
