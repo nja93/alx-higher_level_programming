668c34a04292bcf2b9e4f758ef25f1cd76e28439
nja93 <njanjalchege@gmail.com>
2023-06-12 15:35:22 -0700

    MyInt is a rebel. MyInt has == and != operators inverted

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,6 +8,12 @@ MyInt class is created methods to be inverted
 class MyInt(int):
     """
     Checks for inversion
+
+    args:
+    __equ__(self, other)- inverts the '==' sign
+    __notequ__(self, other)- inverts the '!=' sign
+
+    return: inverse
     """
     def __equ__(self, other):
         """
