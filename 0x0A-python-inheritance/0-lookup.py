feb9b013f3d0566b4c43e242278a764697a8baf9
nja93 <njanjalchege@gmail.com>
2023-06-12 04:39:51 -0700

    function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 
-"""
-definne lookup function
-"""
+"""define lookup function"""
 
 def lookup(obj):
-    """
-    dir()fn that returns a sorted list id banesm including attributesm methods and other items
-    """
+    """  dir()fn that returns a sorted list id banesm including attributesm methods and other items"""
     return dir(obj)
