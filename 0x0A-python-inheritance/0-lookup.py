973a41e3acbabba22e4f1d8015b99294c83fd806
nja93 <njanjalchege@gmail.com>
2023-06-12 04:34:36 -0700

    function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""
+definne lookup function
+"""
+
+def lookup(obj):
+    """
+    dir()fn that returns a sorted list id banesm including attributesm methods and other items
+    """
+    return dir(obj) 
