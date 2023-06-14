a1ba3b6b6673ee5a560c426af274027de2be5b1c
nja93 <njanjalchege@gmail.com>
2023-06-14 11:39:00 -0700

    class Student that defines a student by

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 
 """
-create a class Student
+create class Student
 """
 
-    
+
 class Student:
-    """Defines a class Student"""
+    """
+    Creates class Student
+
+    args:
+        def __init__ - constructor
+        def to_json
+    """
     def __init__(self, first_name, last_name, age):
-        """initializes the attributes of class Student"""
+        """
+        initializes the attributes of class Student, constructor
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
+        """
+        returns dict repr of instance
+        """
         if attrs is None:
             return vars(self)
         else:
-            attribute_dict = {}
+            attribute_dic = {}
             for attribute in attrs:
                 if hasattr(self, attribute):
-                    attribute_dict[attribute] = getattr(self, attribute)
-            return (attribute_dict)
+                    attribute_dic[attribute] = getattr(self, attribute)
+            return attribute_dic
