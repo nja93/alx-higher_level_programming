49e4d77d2f781c59bd456a57d171d4eca4814cd2
nja93 <njanjalchege@gmail.com>
2023-06-14 11:06:30 -0700

    Write a class Square that inherits from Rectangle

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,6 +8,6 @@ Class to JSON
 def class_to_json(obj):
     """
     returns dictionary desc for JSON serialization of an object
+    can also be written as return obj.__dict__
     """
-
-    return vars(obj) """can also be written as return obj.__dict__"""
+    return vars(obj)
