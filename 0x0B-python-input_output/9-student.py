acd9fde6671cb74646d651ff72104f4fc6803b96
nja93 <njanjalchege@gmail.com>
2023-06-14 11:11:57 -0700

    Write a class Student that defines a student by name, age

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Write a class"""
+
+
+class Student:
+    """
+    Defines a class Student
+   
+    args:
+        def __init__ - constructor
+        def to_json
+   
+    """
+    def __init__(self, first_name, last_name, age):
+        """initializes the attributes of class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
