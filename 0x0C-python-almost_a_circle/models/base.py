735febb1c3db88049e93f1d7d3675e088fa40c9b
nja93 <njanjalchege@gmail.com>
2023-06-17 18:12:41 +0000

    first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Write the first class Base"""
+
+class Base:
+    """The base(super) class, private attribute initalized to 0"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializing the base instance"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
