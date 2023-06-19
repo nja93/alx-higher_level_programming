d01caed7062663c72565de353182a6b31b56ec08
nja93 <njanjalchege@gmail.com>
2023-06-19 15:51:58 -0700

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,11 +16,10 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+
     @property
     def width(self):
-        """
-        private attri width
-        """
+        """Retrive attribute width"""
         return self.__width
 
     @width.setter
@@ -33,37 +32,39 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """
-        private attri height
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setting and validating"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """Retrieve """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """set and validate x"""
         if type(value) is not int:
-            raise TypeError("x be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """Retrieve y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setting and validating y"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
