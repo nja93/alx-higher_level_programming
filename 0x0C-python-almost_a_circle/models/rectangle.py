470af4b85d12c0d0adb7da505c0e45cc39a1ed55
nja93 <njanjalchege@gmail.com>
2023-06-19 13:45:45 -0700

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ import a class Base
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """initialize the Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -60,7 +61,7 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-        
+
     @y.setter
     def y(self, value):
         if type(value) != int:
@@ -68,13 +69,13 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-    
 
     def area(self):
         """
         computes area
         """
         return self.width * self.height
+
     def display(self):
         """
         prints result using # in std out
@@ -84,7 +85,7 @@ class Rectangle(Base):
         for h in range(self.__height):
             print("{}{}".format(" " * self.__x, "#" * self.__width), end="")
             print()
-    
+
     def __str__(self):
         """
         String representation of rec`
@@ -113,19 +114,3 @@ class Rectangle(Base):
             "height": self.__height,
             'width': self.__width,
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,10 +3,11 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """class square created as a subclass of Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-         super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
