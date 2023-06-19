414eb02c969c80c2ed70e2743940022ba8ba5dca
nja93 <njanjalchege@gmail.com>
2023-06-19 09:59:59 -0700

    fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,13 +2,14 @@
 
 """Write the first class Base"""
 
+
 class Base:
     """The base(super) class, private attribute initalized to 0"""
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initializing the base instance"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
