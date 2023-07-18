ac3b6ece93d4bd9c614400d473ff1b584b46ca78
nja93 <njanjalchege@gmail.com>
2023-07-18 11:54:44 -0700

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Script that all records of the table second_table
+-- Results both the score and the name (in this order)
+-- Records ordered by score (top first)
+
+SELECT score, name
+FROM second_table ORDER BY score DESC; 
