0d42cbfb03fe65f650bcea7eade8942e40ad1861
nja93 <njanjalchege@gmail.com>
2023-07-18 12:07:24 -0700

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- script that prints the full description of the table first_table from hbtn_0c_0
+-- Script that prints the full description of the table first_table from hbtn_0c_0
 -- The database name will be passed as an argument of the mysql command
 -- do not use the DESCRIBE or EXPLAIN statements
 
