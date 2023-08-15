3607d559e93171bf0694ef8b5bda6dc262060072
nja93 <njanjalchege@gmail.com>
2023-08-15 02:02:39 +0000

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const helloC = 'C is fun';
+const hellopy = 'Python is cool';
+const hellojs = 'JavaScript is amazing';
+
+console.log(helloC);
+console.log(hellopy);
+console.log(hellojs);
