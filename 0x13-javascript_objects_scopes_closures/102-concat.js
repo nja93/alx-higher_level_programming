3d9df356d7c5b4628d47bedd5d241f679b9601ce
nja93 <njanjalchege@gmail.com>
2023-08-17 05:47:38 +0300

    script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-// script that concats 2 files.
-
-// The first argument is the file path of the first source file
-// The second argument is the file path of the second source file
-// he third argument is the file path of the destination
-
 const fs = require('fs');
 const src1 = fs.readFileSync(process.argv[2], 'utf8');
 const src2 = fs.readFileSync(process.argv[3], 'utf8');
 fs.writeFileSync(process.argv[4], src1 + src2);
+
+// The first argument is the file path of the first source file
+// The second argument is the file path of the second source file
+// he third argument is the file path of the destination
