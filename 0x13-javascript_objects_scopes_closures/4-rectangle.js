9688a432aeef34570f85895c3d2603724c81b71f
nja93 <njanjalchege@gmail.com>
2023-08-17 04:03:53 +0300

    now instance method called rotate() that exchanges the width and the height of the rectangle, and double() that doubles

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      let row = '';
+      for (let j = 0; j < this.height; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
