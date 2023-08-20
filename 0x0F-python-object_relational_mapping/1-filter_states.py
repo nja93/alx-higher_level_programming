33e49cafc55452cda5ff3e49c68663d49324c529
nja93 <njanjalchege@gmail.com>
2023-08-20 06:56:48 -0700

    cript that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,39 @@
+#!/bin/user/python3
+
+# import my sql module
+import MySQLdb
+
+
+# import argv(list to store sommand line args) from sys module
+from sys import argv
+
+# checks if this is the script being run as main program
+if __name__ == "__main__":
+
+    # est connection to mysql db using variables in the command line args
+    conmysql = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+
+# create a cursor obj used to execute SQL queries & fetch frm db
+curobj = conn.cursor()
+
+query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+
+# exucte method() executes the queru
+curobj.execute(query)
+
+# fetched all tows returned by query & store in states_list
+
+states_list = curobj.fetchall()
+
+for state in state_list:
+    print(state)
+
+# closes curdoe and db connection & release resources
+curobj.close()
+conmysql.close()
