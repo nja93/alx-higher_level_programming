a9d6ac40bc2263294e9904242dd98d295e13b22d
nja93 <njanjalchege@gmail.com>
2023-08-20 16:16:12 -0700

    script that takes arguments that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""import my sql module"""
+import MySQLdb
+# import argv(list to store sommand line args) from sys module
+from sys import argv
+# checks if this is the script being run as main program
+if __name__ == "__main__":
+    # est connection to mysql db using variables in the command line args
+    conmysql = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+        )
+    # create a cursor obj used to execute SQL queries & fetch frm db
+    curobj = conmysql.cursor()
+    # execute method() executes the query
+
+    curobj.execute(
+        """
+        SELECT * FROM states  WHERE name = %s
+        ORDER BY states.id ASC
+        """
+        )
+    # CODE ABOVE IS PROTECTED FROM INJECTION
+    """ 'SELECT * FROM states WHERE name =
+    {} ORDER BY id ASC".format(argv[4])'
+    Query at risk of sql injection
+    Recommended - using a parameterized query with placeholders.
+    """
+    # fetched all rows returned by query & store in states_list
+    states_list = curobj.fetchall()
+    for state in states_list:
+        print(state)
+    # closes cursor and db connection & release resources
+    curobj.close()
+    conmysql.close()
