fd37d41a532b7110eb3d4c801f2e004a74fcc075
nja93 <njanjalchege@gmail.com>
2023-08-20 10:04:44 -0700

    fixed?

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-
+'''lists all states from the database hbtn_0e_0_usa'''
 import MySQLdb
-
 from sys import argv
 
-if __name__ == '__main__':
-
-    """
-    argv[0] shebang
-    argv[1] mysql username,
-    argv[2] mysql password
-    argv[3] database name
-    """
-
-    my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
-                            db=argv[3], port=3306)
-
-    my_cursor = my_db.cursor()
-
-    my_cursor.execute("SELECT * FROM states")
-
-    my_data = my_cursor.fetchall()
-
-    for data in my_data:
-        print(data)
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states_list = cur.fetchall()
+    for state in states_list:
+        print(state)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+''''
+class definition of a State and an instance Base = declarative_base()
+'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, String, Column
 
 Base = declarative_base()
 
+
 class State(Base):
+    '''
+    The first attribute should be a table name
+    '''
+    __tablename__ = "states"
 
-#first attribute should be table name
-
-__tablename__ = "states"
-id = Column(Integer, primary_key = True)
-name = Column(String(128), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
