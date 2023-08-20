081940e9b52e9d1d07e2873daebb18c64300c555
nja93 <njanjalchege@gmail.com>
2023-08-20 13:54:39 -0700

    fixed??

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-
-
-# import my sql module
+'''lists all states from the database hbtn_0e_0_usa'''
 import MySQLdb
-
-
-# import argv(list to store sommand line args) from sys module
 from sys import argv
 
-# checks if this is the script being run as main program
 if __name__ == "__main__":
-
-    # est connection to mysql db using variables in the command line args
-    conmysql = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        password=argv[2],
-        db=argv[3],
-        port=3306
-    )
-
-# create a cursor obj used to execute SQL queries & fetch frm db
-curobj = conmysql.cursor()
-
-query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
-
-# exucte method() executes the queru
-curobj.execute(query)
-
-# fetched all tows returned by query & store in states_list
-
-states_list = curobj.fetchall()
-
-for state in states_list:
-    print(state)
-
-# closes curdoe and db connection & release resources
-curobj.close()
-conmysql.close()
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+    states_list = cur.fetchall()
+    for state in states_list:
+        print(state)
+    cur.close()
+    conn.close()
