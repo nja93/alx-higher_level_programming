b277a4c471fc425c655b4d568016732e97e7b58d
nja93 <njanjalchege@gmail.com>
2023-09-02 05:51:28 +0300

    added modules

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,6 +3,11 @@
 Uses the GitHub API to display a GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
 """
+
+import urllib.request
+import sys
+import urllib.error
+
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
