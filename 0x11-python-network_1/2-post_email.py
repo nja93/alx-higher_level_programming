c6c10af102fcabdfd186b016b03287546f2377bd
nja93 <njanjalchege@gmail.com>
2023-09-02 04:52:45 +0300

    Python script that takes in a URL and an email, sends a POST request t

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""This is a script that uses urllib to send a POST request"""
+"""
+Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response (decoded in utf-8to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8
+"""
 
 import sys
 from urllib import request
