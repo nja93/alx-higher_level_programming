2376385e1affc8a35ba7d069370be73c045959ef
nja93 <njanjalchege@gmail.com>
2023-09-27 00:28:23 +0300

    fixed-removed print statement for data

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,12 +5,10 @@ const fs = require('fs');
 // file path is the third argument
 const filePath = process.argv[2];
 // The second argument is the string to write
-const addedcontent = process.argv[3];
+const newstring = process.argv[3];
 
-fs.writeFile(filePath, addedcontent, 'utf-8', (error, addedcontent) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(addedcontent);
+fs.writeFile(filePath, newstring, 'utf-8', (error) => {
+  if (error){
+   console.error(error)
   }
-});
+  });
