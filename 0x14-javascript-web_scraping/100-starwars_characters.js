8b6cd53dfb5fe63ed7b5b7de8c5f92bd0f0f863b
nja93 <njanjalchege@gmail.com>
2023-09-28 01:33:27 +0300

    fixed-semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
 // We are writing a script that prints all characters of a Star Wars movie:
-//So first we need access to the request module so that we can use its methods
- 
+// So first we need access to the request module so that we can use its methods
+
 const requestObj = require('request');
 
 const movieId = process.argv[2];
 
 // construct the url that will give us access to the API
- 
+
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
- // Next we need to use our request obj to make the GET request
- // to the specified URL this will make an API call for our specific movieID
- // We will also check for errors and if there are no errors print the title of the movie
- 
+// Next we need to use our request obj to make the GET request
+// to the specified URL this will make an API call for our specific movieID
+// We will also check for errors and if there are no errors print the title of the movie
+
 requestObj.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
