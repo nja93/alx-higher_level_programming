26a63891d4c018cb00286baed1cffcf5bd4cff1a
nja93 <njanjalchege@gmail.com>
2023-09-27 05:15:55 +0300

    script that displays status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const request = require('request')
-
-const url = process.argv[2]
-
-request(url, (error,response, body) =>
-{if (error){
- console.log(error)
-}else{
- console.log('code:', response.statusCode)
-}}
-);
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
