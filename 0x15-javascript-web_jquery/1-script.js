43bb149c83a6460b60f0e4932b21b8fbb352e20c
nja93 <njanjalchege@gmail.com>
2023-10-07 16:26:04 +0300

    js code that updated header colour using JQuery API

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// jQuery selector to find the <header> element and change its text colord
+	// <header> element using $("header"), and then use the .css() method to change its text color using
+	// color property
+	$("header").css("color","#FF0000");
+});
